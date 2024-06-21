4f5d8c86a1b56e3272126ed7059427a676221264
jjaoguedes <joaoguedes1800@gmail.com>
2024-06-21 10:49:35 -0400

    add: ajustes e avaliação do modelo

--- trabalho_3_IAA/trabalho_3_IAA/main.py
+++ trabalho_3_IAA/trabalho_3_IAA/main.py
@@ -9,6 +9,8 @@ from tensorflow.keras.layers import InputLayer, Dense
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.metrics import confusion_matrix
+import seaborn as sns
+
 
 #Funções para o cálculo das métricas
 def acuracia(tp, tn, fp, fn):
@@ -47,16 +49,15 @@ def validacao_cruzada(model, X, y, n_dobras, n_model):
     historico = []
     #Dividindo o dataset em dobras
     kf = KFold(n_splits=n_dobras, random_state=42, shuffle=True)
-    fold = 0
+    fold = 1
     for train_idx, test_idx in kf.split(X):
         #Divisão dos dados para a validação
         X_train, X_test = X[train_idx], X[test_idx]
         y_train, y_test = y[train_idx], y[test_idx]
-
+        print('Fold = {} Model {}'.format(fold,n_model))
         #Treinando o modelo na dobra atual
         hist = model.fit(X_train, y_train, epochs=150,verbose=0)
         historico.append(hist)
-        fold = fold + 1
 
         #Predizendo as classes
         predictions = model.predict(X_test)
@@ -109,14 +110,14 @@ def validacao_cruzada(model, X, y, n_dobras, n_model):
         mean_f1_score = np.mean(scores_f1_score)
         print('Média da f1_score: {:.4f}'.format(mean_f1_score))
 
-        scores_taxa_de_erro.append(f1_Score)
+        scores_taxa_de_erro.append(taxa_de_erro)
         mean_taxa_de_erro = np.mean(scores_taxa_de_erro)
         print('Média da Taxa de erro: {:.4f}'.format(mean_taxa_de_erro))
 
         accuracy = hist.history['accuracy']
         accuracy_np = np.array(accuracy)
         mean_accuracy_np = np.mean(accuracy_np)
-        print('Média do modelo Accuracy: {:.4f}'.format(mean_accuracy_np))
+        print('Média do model Accuracy: {:.4f}'.format(mean_accuracy_np))
         list_mean_accuracy.append(mean_accuracy_np)
 
         loss = hist.history['loss']
@@ -129,19 +130,22 @@ def validacao_cruzada(model, X, y, n_dobras, n_model):
         #Mostrar curva de convergência da acúracia em função das épocas
         for history in historico:
             plt.plot(history.history['accuracy'])
-        plt.title(f'Curva de Convergência da Acurácia - Arquitetura {n_model} fold = ' + str(fold))
+        plt.title(f'Curva de Convergência da Acurácia - Arquitetura {n_model}')
         plt.xlabel('Épocas')
         plt.ylabel('Acurácia')
-        plt.savefig('history_accuracy_fold' + str(fold) + '.png')
+        plt.savefig('history_accuracy_fold_' + str(fold) + '_model_' + str(n_model) + '.png')
         plt.show()
 
         for history in historico:
             plt.plot(history.history['loss'])
-        plt.title(f'Curva de Convergência da Loss - Arquitetura {n_model} fold = ' + str(fold))
+        plt.title(f'Curva de Convergência da Loss - Arquitetura {n_model}')
         plt.xlabel('Épocas')
         plt.ylabel('Loss')
-        plt.savefig('history_loss_fold' + str(fold) + '.png')
+        plt.savefig('history_loss_fold_' + str(fold) + '_model_' + str(n_model) + '.png')
         plt.show()
+        print('-'*100)
+        fold = fold + 1
+    return model
 
 
 
@@ -172,29 +176,32 @@ np.unique(y)
 encoded = tf.keras.utils.to_categorical(y)
 #print(encoded)
 
+#Utilizar o train_test_split do sklearn para dividir em treino e teste
+x_train, x_test, y_train, y_test = train_test_split(X, encoded, test_size=0.2, random_state=0)
+
 #Build the Model
 #Utilizar a classe Sequential()
 #Definir as camadas InputLayer e Dense
 #Mostrar a arquitetura
 model1 = Sequential([InputLayer(input_shape=(4,)),
-                    Dense(units=15, activation='relu'),
-                    Dense(units=15, activation='relu'),
+                    Dense(units=13, activation='relu'),
+                    Dense(units=13, activation='relu'),
                     Dense(units=2, activation='softmax')])
 
 print(model1.summary())
 
 model2 = Sequential([InputLayer(input_shape=(4,)),
-                    Dense(units=20, activation='relu'),
-                    Dense(units=20, activation='relu'),
+                    Dense(units=16, activation='relu'),
+                    Dense(units=16, activation='relu'),
                     Dense(units=2, activation='softmax')])
 
 print(model2.summary())
 
 #Compilar o modelo
 #Config otimizador, função de perda e métricas
-model1.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=0.01), loss='categorical_crossentropy',
+model1.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=0.0001), loss='categorical_crossentropy',
                       metrics=['accuracy'])
-model2.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=0.01), loss='categorical_crossentropy',
+model2.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=0.0001), loss='categorical_crossentropy',
                       metrics=['accuracy'])
 #Train the Model
 #Chamar a função fit() para treinar
@@ -203,9 +210,52 @@ model2.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=0.01), loss='cate
 
 fold = 5
 # Realizando validação cruzada com 5 dobras
-validacao_cruzada(model1, X, encoded, n_dobras= fold, n_model=1)
+model1_fit = validacao_cruzada(model1, X, encoded, n_dobras=fold, n_model=1)
 
-validacao_cruzada(model2, X, encoded, n_dobras= fold, n_model=2)
+model2_fit = validacao_cruzada(model2, X, encoded, n_dobras=fold, n_model=2)
+
+#Avaliando os modelos
+predictions_1 = model1.predict(x_test)
+predictions_2 = model2.predict(x_test)
+
+print('predictions_1[25] = {}'.format(predictions_1[25]))
+print('y_test[25] = {}'.format(y_test[25]))
+
+print('predictions_2[25] = {}'.format(predictions_2[25]))
+print('y_test[25] = {}'.format(y_test[25]))
+
+y_pred1 = np.argmax(predictions_1, axis=1)
+y_pred2 = np.argmax(predictions_2, axis=1)
+
+y_test1 = np.argmax(y_test, axis=1)
+
+#criar matrizes de confusão para cada modelo
+cm1 = confusion_matrix(y_test1, y_pred1)
+cm2 = confusion_matrix(y_test1, y_pred2)
+
+labels = ['maligno',
+          'benigno',]
+
+plt.subplots(figsize=(8,8))
+sns.heatmap(cm1, cmap='YlGnBu', annot=True, cbar=False, xticklabels=labels, yticklabels=labels)
+plt.title('Confusion Matrix Model 1')
+plt.xlabel('Predict')
+plt.ylabel('True')
+plt.savefig('cm_model_1.png')
+
+
+plt.subplots(figsize=(8,8))
+sns.heatmap(cm2, cmap='YlGnBu', annot=True, cbar=False, xticklabels=labels, yticklabels=labels)
+plt.title('Confusion Matrix Model 2')
+plt.xlabel('Predict')
+plt.ylabel('True')
+plt.savefig('cm_model_2.png')
+
+
+
+
+
+
 
 
 
